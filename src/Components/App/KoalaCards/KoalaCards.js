6694b8f539e6b05cc78ed7d3b339277d6b954ec7
Joe Evert <joeevert@gmail.com>
2018-12-06 13:49:35 -0600

    card comp file setup

---
+++ src/Components/App/KoalaCards/KoalaCards.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+// material ui
+import Card from '@material-ui/core/Card';
+
+class KoalaCards extends Component {
+  render() {
+    return (
+      <div>
+        <Card>
+
+        </Card>
+      </div>
+    );
+  }
+}
+
+export default KoalaCards;
