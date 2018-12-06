17a54574c4bbec37724b04fb6bfa3a0b679ffdae
Jake Wylie Alexios <jwalexios@gmail.com>
2018-12-06 14:04:04 -0600

    further koalaSaga work (addKoalaSaga and markTransferSaga)

--- src/redux/sagas/koalaSaga.js
+++ src/redux/sagas/koalaSaga.js
@@ -2,16 +2,17 @@ import { takeEvery, call, put } from 'redux-saga/effects';
 import axios from 'axios';
 
 function* koalaSaga() {
-    yield takeEvery('SET_KOALAS_SAGA', getKoalasSaga);
-    // yield takeEvery('ADD_KOALA', addKoalaSaga);
-    // yield takeEvery('EDIT_KOALA', editKoalaSaga);
+    yield takeEvery('GET_KOALAS', getKoalasSaga);
+    yield takeEvery('ADD_KOALA', addKoalaSaga);
+    yield takeEvery('MARK_TRANSFER', markTransferSaga);
+    yield takeEvery('EDIT_KOALA', editKoalaSaga);
 }
 
 function* getKoalasSaga() {
     try {
-        const response = yield call(axios.get, '/koala/');
+        const response = yield call(axios.get, '/koala');
 
-        // yield put({type: 'SET_KOALAS', payload: response})
+        yield put({type: 'SET_KOALAS', payload: response})
         yield console.log('getKoalasSaga response:', response);
         
     } catch (error) {
@@ -19,5 +20,30 @@ function* getKoalasSaga() {
         alert('Unable to getKoalasSaga!');  
     }
 }
+
+function* addKoalaSaga() {
+    try {
+        yield call(axios.post, '/koala', action.payload);
+        yield put({type: 'GET_KOALAS'});
+
+    } catch (error) {
+        console.log(error);
+        alert('Unable to addKoalaSaga!');
+        
+    }
+}
+
+function* markTransferSaga() {
+    let koalaId = action.payload;
+    try {
+        yield call(axios.put, `/koala/${koalaId}`);
+        yield put({type: 'GET_KOALAS'});
+    } catch (error) {
+        console.log(error);
+        alert('Unable to markTransferSaga!');
+        
+    }
+}
+
 
 export default koalaSaga;
